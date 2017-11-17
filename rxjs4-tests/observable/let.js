3bad5bf81c9ff172421ad7a5c1e9b55688329742
Ashley Cutmore <acutmore@users.noreply.github.com>
2017-11-17 17:56:06 +0000

    test(backwards-compatibility): RxJs4 unit tests added
    * unit tests copied over from rxjs4 repo
    * reactive assert Rx.internals polyfilled in unit tests

---
+++ rxjs4-tests/observable/let.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+(function () {
+  /* jshint undef: true, unused: true */
+  /* globals QUnit, test, Rx, ok */
+  QUnit.module('let');
+
+  var Observable = Rx.Observable;
+
+  test('let calls function immediately', function () {
+    var called = false;
+
+    Observable.empty()['let'](function (x) {
+      called = true;
+      return x;
+    });
+
+    ok(called);
+  });
+})();
