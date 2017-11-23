1ca5f361b0f60506ec109620fc07c302667393d4
Ashley Cutmore <acutmore@users.noreply.github.com>
2017-11-23 12:32:30 +0000

    feat(Observable): added Observable.fromArray

--- src/add/observable/backwards-compatibility-static.ts
+++ src/add/observable/backwards-compatibility-static.ts
@@ -1,12 +1,19 @@
 // v4-backwards-compatibility
 
 import { Observable } from '../../Observable';
+
 import { of as staticOf } from '../../observable/of';
-
 Observable.just = staticOf;
-
 declare module '../../Observable' {
   namespace Observable {
     export let just: typeof staticOf; //formOf an iceberg!
+  }
+}
+
+import {from} from '../../observable/from';
+Observable.fromArray = from;
+declare module '../../Observable' {
+  namespace Observable {
+    export let fromArray: typeof from;
   }
 }
