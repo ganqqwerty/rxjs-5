e934cfb422ee7fbd0217add3847fbe829fd524c1
Ashley Cutmore <acutmore@users.noreply.github.com>
2018-01-02 12:23:39 +0000

    feat(Operators): .shareReplay now has rxjs 4 behavior for backwards compatibility

--- src/add/operator/shareReplay.ts
+++ src/add/operator/shareReplay.ts
@@ -2,10 +2,16 @@
 import { Observable } from '../../Observable';
 import { shareReplay } from '../../operator/shareReplay';
 
-Observable.prototype.shareReplay = shareReplay;
+// v4-backwards-compatibility
+Observable.prototype.shareReplay_persisted = shareReplay;
+Observable.prototype.shareReplay = function<T>(this: Observable<T>, count: number): Observable<T> {
+  return this.publishReplay(count).refCount();
+};
 
 declare module '../../Observable' {
   interface Observable<T> {
+    // v4-backwards-compatibility
     shareReplay: typeof shareReplay;
+    shareReplay_persisted: typeof shareReplay;
   }
 }
